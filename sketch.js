794e4d9fc1b4cf08613b988dae211f570afd2bdb
khendersonPC <20666083+khendersonPC@users.noreply.github.com>
2021-02-07 10:11:49 -0700

    changed navbar, stand along canvas page

--- sketch.js
+++ sketch.js
@@ -3,16 +3,16 @@
 
 
 var pos = 0;
-var pos1=10;
-var posXX=0;
+var pos1 = 10;
+var posXX = 0;
 // setup config variables and start the program
-function init () {
+function init() {
   canvas = document.getElementById('myCanvas')
-  var heightRatio=.48;
-  canvas.height=canvas.width*heightRatio;
-  
+  var heightRatio = .48;
+  canvas.height = canvas.width * heightRatio;
+
   ctx = canvas.getContext('2d')
- 
+
   var s0 = new Rectangle(0, 0, 50, 200, '#93BFBB');
   var s1 = new Rectangle(0, 220, 50, 200, '#93BFBB');
   var s2 = new Rectangle(0, 440, 150, 60, '#F1F2C4');
@@ -25,46 +25,6 @@ function init () {
   var s8 = new Rectangle(510, 0, 140, 140, '#F1F2C4');
   var s9 = new Rectangle(510, 160, 140, 140, '#F1F2C4');
 
- s0.draw();
- s1.draw();
- s2.draw();
- s3.draw();
- s4.draw();
- s5.draw();
- s6.draw();
- s7.draw();
- s8.draw();
- s9.draw();
-
- ctx.font = "25px Verdana";
- ctx.fillStyle = "white";
-ctx.fillText("Hello, I am Kelly Henderson.", 580, 380);
-ctx.fillText("I am a full stack developer.", 580, 420);
-
-}
-
-// wait for the HTML to load
-document.addEventListener('DOMContentLoaded', init)
-
-function move(){
-//text
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.font = "25px Verdana";
- ctx.fillStyle = "white";
-ctx.fillText("Hello, I am Kelly Henderson.", 580, 380);
-ctx.fillText("I am a full stack developer.", 580, 420);
-
-  var s0 = new Rectangle(0, pos, 50, 200, '#93BFBB');
-  var s1 = new Rectangle(posXX, 220, 50, 200, '#93BFBB');
-  var s2 = new Rectangle(0, pos+440, 150, 60, '#F1F2C4');
-  var s3 = new Rectangle(70, 120, 300, 300, '#2C4373');
-  var s4 = new Rectangle(70, pos*-1, 300, 100, '#A66963');
-  var s5 = new Rectangle(390, pos, 100, 300, '#A66963');
-  var s6 = new Rectangle((posXX*-1)+170, 440, 200, 60, '#A66963');
-  var s7 = new Rectangle(390, (pos*-1)+320, 140, 200, '#93BFBB');
-  var s8 = new Rectangle(510, 0, 140, 140, '#F1F2C4');
-  var s9 = new Rectangle(510+posXX, 160, 140, 140, '#F1F2C4');
-  
   s0.draw();
   s1.draw();
   s2.draw();
@@ -75,9 +35,49 @@ ctx.fillText("I am a full stack developer.", 580, 420);
   s7.draw();
   s8.draw();
   s9.draw();
-  pos+=event.deltaY;
-  posXX-=event.deltaY;
-  
+
+  ctx.font = "25px Verdana";
+  ctx.fillStyle = "white";
+  ctx.fillText("Hello, I am Kelly Henderson.", 580, 380);
+  ctx.fillText("I am a full stack developer.", 580, 420);
+
+}
+
+// wait for the HTML to load
+document.addEventListener('DOMContentLoaded', init)
+
+function move() {
+  //text
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.font = "25px Verdana";
+  ctx.fillStyle = "white";
+  ctx.fillText("Hello, I am Kelly Henderson.", 580, 380);
+  ctx.fillText("I am a full stack developer.", 580, 420);
+
+  var s0 = new Rectangle(0, pos, 50, 200, '#93BFBB');
+  var s1 = new Rectangle(posXX, 220, 50, 200, '#93BFBB');
+  var s2 = new Rectangle(0, pos + 440, 150, 60, '#F1F2C4');
+  var s3 = new Rectangle(70, 120, 300, 300, '#2C4373');
+  var s4 = new Rectangle(70, pos * -1, 300, 100, '#A66963');
+  var s5 = new Rectangle(390, pos, 100, 300, '#A66963');
+  var s6 = new Rectangle((posXX * -1) + 170, 440, 200, 60, '#A66963');
+  var s7 = new Rectangle(390, (pos * -1) + 320, 140, 200, '#93BFBB');
+  var s8 = new Rectangle(510, 0, 140, 140, '#F1F2C4');
+  var s9 = new Rectangle(510 + posXX, 160, 140, 140, '#F1F2C4');
+
+  s0.draw();
+  s1.draw();
+  s2.draw();
+  s3.draw();
+  s4.draw();
+  s5.draw();
+  s6.draw();
+  s7.draw();
+  s8.draw();
+  s9.draw();
+  pos += event.deltaY;
+  posXX -= event.deltaY;
+
 
 }
 
@@ -86,7 +86,7 @@ class Rectangle {
   // you create new Rectangles by calling this as a function
   // these are the arguments you pass in
   // add default values to avoid errors on empty arguments
-  constructor (
+  constructor(
     x = 0, y = 0,
     width = 0, height = 0,
     fillColor = ''
@@ -102,30 +102,30 @@ class Rectangle {
 
 
   // gets the X position of the left side
-  get left () {
+  get left() {
     // origin is at top left so just return x
     return this.x
   }
 
   // get X position of right side
-  get right () {
+  get right() {
     // x is left position + the width to get end point
     return this.x + this.width
   }
 
   // get the Y position of top side
-  get top () {
+  get top() {
     // origin is at top left so just return y
     return this.y
   }
 
   // get Y position at bottom
-  get bottom () {
+  get bottom() {
     return this.y + this.height
   }
 
   // draw rectangle to screen
-  draw () {
+  draw() {
     // destructuring
     const {
       x, y, width, height,
@@ -153,5 +153,4 @@ class Rectangle {
     ctx.restore()
   }
 }
- 
 
